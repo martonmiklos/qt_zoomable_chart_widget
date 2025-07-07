ae1a0246c423854570fb594d31c0ed00861fcc9d
Miklos Marton <martonmiklosqdev@gmail.com>
2025-07-07 15:36:28 +0200

    - Add missing overrides - Fix Qt 6.8 deprecation warnings

--- zoomablechartview.h
+++ zoomablechartview.h
@@ -35,11 +35,11 @@ public:
 
 
 protected:
-    void mousePressEvent(QMouseEvent *event);
-    void mouseMoveEvent(QMouseEvent *event);
-    void mouseReleaseEvent(QMouseEvent *event);
-    void keyPressEvent(QKeyEvent *event);
-    void wheelEvent(QWheelEvent *event);
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void keyPressEvent(QKeyEvent *event) override;
+    void wheelEvent(QWheelEvent *event) override;
     //![2]
 
 private:
